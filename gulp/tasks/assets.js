99e90e5cd1a094dc154818dd328fb1060c8d9fb9
Eric Fuhrmann <ekfuhrmann@gmail.com>
2018-02-02 10:57:14 -0500

    Update 2018-02-02T15:57:12.184Z

--- gulp/tasks/assets.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const gulp = require('gulp')
-const config = require('../config')
-
-gulp.task('assets', () => {
-    return gulp.src(config.assets)
-    .pipe(gulp.dest(`${config.distFolder}/assets/`))
-})
